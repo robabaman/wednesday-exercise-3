ebd2fd2d63872ad46b687b318e28614e12c4fa64
robabaman <robabaman@gmail.com>
2025-09-04 20:32:38 +0200

    Add project files.

---
+++ wednesday exercise 3/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I like to code in C#!");
+Console.WriteLine("I am nameless.");
